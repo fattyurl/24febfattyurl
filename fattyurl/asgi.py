8af82f67f2cb6f6c4fce75bb7c3344cdba1c7b7f
RVDavra <riteshdavra@gmail.com>
2026-02-24 21:50:48 +0530

    setup whitenoise

--- fattyurl/asgi.py
+++ fattyurl/asgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.asgi import get_asgi_application
 
+# WhiteNoise is configured via middleware in fattyurl/settings.py.
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fattyurl.settings')
 
 application = get_asgi_application()
